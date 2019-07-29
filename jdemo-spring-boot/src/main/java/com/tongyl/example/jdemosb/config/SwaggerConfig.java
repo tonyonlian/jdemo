0adfaef6f7580fd2b6d898e7e3d4410ccea7cdad
tonyonlian <540340102@qq.com>
2019-07-29 14:07:00 +0800

    增加swagger的集成

---
+++ jdemo-spring-boot/src/main/java/com/tongyl/example/jdemosb/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.tongyl.example.jdemosb.config;
+
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;;
+
+/**
+ * @author Created by Tunyl on 2019/7/29.
+ * @version 1.0
+ */
+
+@Configuration
+@EnableSwagger2
+@ConditionalOnExpression("${swagger.enable}")
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.tongyl.example.jdemosb"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("tongyl-demo系统")
+               // .description("powered by tongyl")
+               // .termsOfServiceUrl("https://www.tongyl.cn/")
+                //.contact(contact)
+                .version("1.0")
+                .build();
+    }
+
+
+
+}
